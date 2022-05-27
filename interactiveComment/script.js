b548e64004af1260df7980111166fd296c9a5c8c
dodytrifa <dody.trifa@gmail.com>
2022-05-27 22:06:39 +0700

    add comment functionality

--- interactiveComment/script.js
+++ interactiveComment/script.js
@@ -5,3 +5,19 @@ const openModal = document.querySelector('.open-btn')
 openModal.addEventListener('click', ()=>{
   modal.showModal()
 })
+
+//reply functionality
+const showInput = document.querySelectorAll('.reply')
+
+showInput.forEach((btn) => 
+  btn.addEventListener("click",(e)=>{
+    let parentDiv = e.target.closest('.comment__container');
+
+    let idDiv = parentDiv.id
+    
+    if(idDiv){
+      let childDiv = parentDiv.querySelectorAll(`[dataset=${idDiv}]`)
+      childDiv.forEach((child)=> child.classList.toggle("opened"))
+    }
+  })
+)
